095953cb5d36034f9b12bc497a4c0524411c6dfe
Avani-158 <avanimittal15@gmail.com>
2026-07-24 03:25:21 +0000

    Add inversion pairs solution

---
+++ programming/inversion_pairs.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    long long count = 0;
+
+
+    int j = mid + 1;
+    for (int i = left; i <= mid; i++) {
+        while (j <= right && (long long)arr[i] > 2LL * arr[j])
+            j++;
+        count += (j - (mid + 1));
+    }
+
+    int i = left;
+    j = mid + 1;
+    int k = left;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+
+    return count;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long count = 0;
+
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        count += mergeSort(arr, temp, left, mid);
+        count += mergeSort(arr, temp, mid + 1, right);
+        count += merge(arr, temp, left, mid, right);
+    }
+
+    return count;
+}
+
+long long reversePairs(int arr[], int n) {
+    int *temp = (int *)malloc(n * sizeof(int));
+    long long ans = mergeSort(arr, temp, 0, n - 1);
+    free(temp);
+    return ans;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%lld\n", reversePairs(arr, n));
+
+    return 0;
+}
