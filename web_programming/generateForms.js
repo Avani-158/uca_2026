cd45617d1dad5ba628657faa8944faa2d8a58d55
Avani-158 <avanimittal15@gmail.com>
2026-08-02 21:11:17 +0530

    Generate multiple form fields

---
+++ web_programming/generateForms.js
@@ -0,0 +1,14 @@
+const fields = [ 
+  "First Name", 
+  "Last Name", 
+  "Email"
+]; 
+
+function generateForm() {
+    return fields.map(field => 
+        `<label>${field}</label>\n 
+        <input type='text'>`
+    ).join('\n');
+}
+
+console.log(generateForm());
