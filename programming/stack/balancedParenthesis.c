380b47d503aabeda8090cc9b46be62c03716f93f
Avani-158 <avanimittal15@gmail.com>
2026-08-24 19:27:06 +0530

    Add balanced parenthesis

--- programming/stack/balancedParenthesis.c
+++ programming/stack/balancedParenthesis.c
@@ -1,77 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-typedef struct {
-    char *data;
-    int top;
-    int capacity;
-} Stack;
+#define MAX 30
 
-void init(Stack *s, int capacity) {
-    s->capacity = capacity;
-    s->top = -1;
-    s->data = (char *)malloc(capacity * sizeof(char));
+char stack[MAX];
+int top = -1;
+
+void push(char ch) {
+    stack[++top] = ch;
 }
 
-void push(Stack *st, char ch) {
-    if (st->top == st->capacity - 1) {
-        st->capacity *= 2;
-        st->data = (char *)realloc(st->data, st->capacity * sizeof(char));
-    }
-
-    st->data[++(st->top)] = ch;
+char pop() {
+    return stack[top--];
 }
 
-char pop(Stack *st) {
-    if (st->top == -1) return '\0';
-    return st->data[(st->top)--];
-}
-
-int isMatch(char open, char close) {
+bool isMatching(char open, char close) {
     return (open == '(' && close == ')') ||
            (open == '{' && close == '}') ||
            (open == '[' && close == ']');
 }
 
-int isBalanced(char *expr) {
-    Stack s;
-    init(&s, 10);
+int main() {
+    char expression[MAX + 1];
 
-    for (int i = 0; expr[i] != '\0'; i++) {
-        char ch = expr[i];
+    scanf("%30s", expression);
 
-        if (ch == '(' || ch == '[' || ch == '{') {
-            push(&s, ch);
-        } 
-        else if (ch == ')' || ch == ']' || ch == '}') {
-            if (s.top == -1) {
-                free(s.data);
+    for (int i = 0; i < strlen(expression); i++) {
+
+        char ch = expression[i];
+
+        if (ch == '(' || ch == '{' || ch == '[') {
+            push(ch);
+        }
+
+        else if (ch == ')' || ch == '}' || ch == ']') {
+
+            if (top == -1) {
+                printf("false");
                 return 0;
             }
-            char top = pop(&s);
-            if (!isMatch(top, ch)) {
-                free(s.data);
+
+            char open = pop();
+
+            if (!isMatching(open, ch)) {
+                printf("false");
                 return 0;
             }
         }
     }
 
-    int res = (s.top == -1);
-    free(s.data);
-    return res;
-}
-
-int main() {
-    char expr[100];  
-
-    printf("Enter expression: ");
-    scanf("%s", expr);
-
-    if (isBalanced(expr))
-        printf("true\n");
+    if (top == -1)
+        printf("true");
     else
-        printf("false\n");
+        printf("false");
 
     return 0;
 }
